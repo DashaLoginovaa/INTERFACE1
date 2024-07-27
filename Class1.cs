a15b642b50a69f533e853ae4b341a69acfd00746
SkillFactoryCSharpTest <dashulichka311214@gmail.com>
2024-07-27 11:48:28 +0700

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTERFACE1
+{
+    internal class Calculator: ICalculator
+    {
+        public double Calculate()
+        {
+            Console.WriteLine("Введите два числа");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = x + y;
+            Console.WriteLine("Сумма равна" + " " + z);
+            return z;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTERFACE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            while (true)
+            {
+                try
+                {
+                    calculator.Calculate();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введено некорректное значение");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
